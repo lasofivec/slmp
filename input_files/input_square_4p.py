9124cb1bbc15ea4808abf8ffa8ef496dcd26bcad
Laura Mendoza <mela@ipp.mpg.de>
2016-08-17 10:18:31 +0200

    [SLMP] updated the 4p square input file

--- input_files/input_square_4p.py
+++ input_files/input_square_4p.py
@@ -7,29 +7,29 @@ DEBUG_MODE = False
 # *********************************************
 domain = SQUARE_4p_domain
 # Discretization:
-NPTS1 = 50
-NPTS2 = 50
+NPTS1 = 100
+NPTS2 = 100
 
 # *********************************************
 # Definition of simulation
 # *********************************************
-tmax = 5.
-dt   = 0.025
-viewstep = 1
+tmax = 9.
+dt   = 0.05
+viewstep = 10
 
 # *********************************************
 # Definition of initial distribution
 # *********************************************
 which_f = "GAUSSIAN_DISTRIBUTION"
-dist_center_x = 0.15
-dist_center_y = 0.
+dist_center_x = -0.5
+dist_center_y = -0.5
 
 # *********************************************
 # Definition of advection coefficient
 # *********************************************
 which_advec = ADVEC_CNST
-advec_dir1 = -0.1
-advec_dir2 = 0.
+advec_dir1 = 0.1
+advec_dir2 = 0.1
 
 #**********************************************
 # Definition of interpolation method used
